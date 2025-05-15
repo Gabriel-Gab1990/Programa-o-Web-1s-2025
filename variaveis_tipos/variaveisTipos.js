6ca71cb3e27334a63d173a43accbc0d6ff348791
Gabriel lima Silva uc23201047 <gl271018@gmail.com>
2025-05-15 10:13:02 -0300

    Update variaveisTipos.js

--- variaveis_tipos/variaveisTipos.js
+++ variaveis_tipos/variaveisTipos.js
@@ -1,15 +1,12 @@
 const n = 3;
-//n = 5;
 
 var j = 2;
 var j = 3;
 
-//console.log('j='+j);
 
 let i = 4;
 i = 6;
 
-//console.log('i='+i);
 
 let nome = "Joao";
 let cpf = '123';
@@ -17,14 +14,10 @@ let telefone = '123';
 
 let tString = `Meu nome é ${nome}. Meu cpf é ${cpf}. Telefone: ${telefone}`;
 
-//console.log(tString);
-
 let x = 5.0;
 x = .5;
 x = 3;
 
-//console.log(typeof x);
-//console.log(typeof tString);
 
 let verdadeiro = true;
 let falso = false;
